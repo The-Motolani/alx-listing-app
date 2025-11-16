8c2bb8e06228e6666ec81aa9e86a28bb70b91e91
Motolani <alebiosumotolani@gmail.com>
2025-11-16 17:29:32 +0100

    initial commit

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,16 @@
+export const PROPERTYLISTINGSAMPLE: PropertyProps [] = [
+    {
+        name: 'Villa Ocean Breeze',
+        address: {
+            state: 'Seminyak',
+            city: 'Bali',
+            country: 'Indonesia'
+        },
+        rating: 4.89,
+        category: ['Luxury Villa', 'Pool', 'Free Parking'],
+    }
+]
+
 export const API_BASE_URL = 'https://api.example.com';
 export const APP_NAME = 'ALX Listing App';
+
